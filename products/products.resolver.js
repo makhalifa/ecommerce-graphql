22b502dc4a56fd536a04a26b95bf655a38d6d07d
makhalifa <7mdiano@gmail.com>
2023-07-22 20:44:25 +0300

    mutations

--- products/products.resolver.js
+++ products/products.resolver.js
@@ -2,6 +2,22 @@ const productsModel = require('./products.model');
 
 module.exports = {
   Query: {
-    products: () => productsModel.getAllProducts(),
+    products: () => {
+      return productsModel.getAllProducts();
+    },
+    productsByPrice: (_, args) => {
+      return productsModel.getProductsByPrice(args.min, args.max);
+    },
+    product: (_, args) => {
+      return productsModel.getProductById(args.id);
+    },
+  },
+  Mutation: {
+    addProduct: (_, args) => {
+      return productsModel.addProduct(args.id, args.description, args.price);
+    },
+    addReview: (_, args) => {
+      return productsModel.addReview(args.productId, args.rating, args.comment);
+    },
   },
 };
